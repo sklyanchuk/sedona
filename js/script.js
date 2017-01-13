45cf8129c1fce5abab4681baf0f2364db0074ff2
sklyanchuk <artem.sklyanchuk@gmail.com>
2017-01-13 19:28:31 +0300

    Added functions for plus and minus buttons in search form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-var adultMinusButton = document.getElementById("adult-minus");
-var adultPlusButton = document.getElementById("adult-plus");
-var kidMinusButton = document.getElementById("kid-minus");
-var kidPlusButton = document.getElementById("kid-plus");
-var adults = document.getElementById("adults");
-var kids = document.getElementById("kids");
+var adultMinusButton = document.getElementById('adult-minus');
+var adultPlusButton = document.getElementById('adult-plus');
+var kidMinusButton = document.getElementById('kid-minus');
+var kidPlusButton = document.getElementById('kid-plus');
+var adults = document.getElementById('adults');
+var kids = document.getElementById('kids');
 
-adultMinusButton.addEventListener("click", adultMinus());
-
-adultMinus = function() {
+adultMinusButton.addEventListener('click', adultMinus);
+function adultMinus() {
   event.preventDefault();
-  --adults.value;
-};
+  if (adults.value > 0) {
+    --adults.value;
+  }
+}
+adultPlusButton.addEventListener('click', adultPlus);
+function adultPlus() {
+  event.preventDefault();
+  if (adults.value < 10) {
+    ++adults.value;
+  }
+}
+kidMinusButton.addEventListener('click', kidMinus);
+function kidMinus() {
+  event.preventDefault();
+  if (kids.value > 0) {
+    --kids.value;
+  }
+}
+kidPlusButton.addEventListener('click', kidPlus);
+function kidPlus() {
+  event.preventDefault();
+  if (kids.value < 10) {
+    ++kids.value;
+  }
+}

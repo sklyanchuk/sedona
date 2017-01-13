ed1eb196a61b076db68ad85796c506b249701349
sklyanchuk <artem.sklyanchuk@gmail.com>
2017-01-13 20:43:33 +0300

    Added disabled style for search form buttons

--- js/script.js
+++ js/script.js
@@ -8,28 +8,44 @@ var kids = document.getElementById('kids');
 adultMinusButton.addEventListener('click', adultMinus);
 function adultMinus() {
   event.preventDefault();
-  if (adults.value > 0) {
+  adultPlusButton.classList.remove('btn-type-qty--disabled');
+  if (adults.value > 1) {
     --adults.value;
+  } else if (adults.value > 0) {
+    --adults.value;
+    adultMinusButton.classList.add('btn-type-qty--disabled');
   }
 }
 adultPlusButton.addEventListener('click', adultPlus);
 function adultPlus() {
   event.preventDefault();
-  if (adults.value < 10) {
+  adultMinusButton.classList.remove('btn-type-qty--disabled');
+  if (adults.value < 9) {
     ++adults.value;
+  } else if (adults.value < 10) {
+    ++adults.value;
+    adultPlusButton.classList.add('btn-type-qty--disabled');
   }
 }
 kidMinusButton.addEventListener('click', kidMinus);
 function kidMinus() {
   event.preventDefault();
-  if (kids.value > 0) {
+  kidPlusButton.classList.remove('btn-type-qty--disabled');
+  if (kids.value > 1) {
     --kids.value;
+  } else if (kids.value > 0) {
+    --kids.value;
+    kidMinusButton.classList.add('btn-type-qty--disabled');
   }
 }
 kidPlusButton.addEventListener('click', kidPlus);
 function kidPlus() {
   event.preventDefault();
-  if (kids.value < 10) {
+  kidMinusButton.classList.remove('btn-type-qty--disabled');
+  if (kids.value < 9) {
     ++kids.value;
+  } else if (kids.value < 10) {
+    ++kids.value;
+    kidPlusButton.classList.add('btn-type-qty--disabled');
   }
 }
